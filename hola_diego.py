91313dffd2a5f8ec490d4253e50aabf697a39097
Franco More <franco.more1758@gmail.com>
2014-05-07 19:46:25 -0300

    lo modifique

---
+++ hola_diego.py
@@ -0,0 +1,2 @@
+[ui]
+usename _ diego olmos <tecnodiego23@gmail.com>
---
+++ hola_romina.py
@@ -0,0 +1,6 @@
+nombre=raw_input ("ingrese su nombre");
+print "hola mundo" 
+print nombre
+numero=input("ingrese un numero");
+if numero == 50:
+    print "alto"
